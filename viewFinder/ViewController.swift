54a62a1aba0c803f7860a6640216d36f3d3fa3a1
Matt Wecht <mattwecht@gmail.com>
2019-06-21 18:44:47 -0600

    Initial Commit

---
+++ viewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewFinder
+//
+//  Created by Matthew Wecht on 6/21/19.
+//  Copyright © 2019 Matthew Wecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
